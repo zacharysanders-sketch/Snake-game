aa3579172621d42df73cc5a8c97a2845f177c46b
zacharysanders-sketch <zachary.sanders@bsd52.org>
2026-04-14 13:05:33 -0700

    Reorganize draw.js for better structure
    Refactor game setup and initialization logic.

--- draw.js
+++ draw.js
@@ -1,20 +1,11 @@
-(function setup() {
-    snake = new Snake();
-    fruit = new Fruit();
-    fruit.pickLocation();
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
 
-    window.setInterval(() => {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        fruit.draw();
-        snake.update();
-        snake.draw();
-        if (snake.eat(fruit)) {
-            fruit.pickLocation();
-        }
-        snake.checkCollision();  
-        document.querySelector('#score').innerText = snake.total;
-    }, 250);
-}());
+var snake;
+var fruit;
 
 window.addEventListener('keydown', ((evt) => {
     const direction = evt.key.replace('Arrow', '');
