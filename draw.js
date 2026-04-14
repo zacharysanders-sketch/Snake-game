6b797960d979c36be94a992f097ce08aa6d1b557
zacharysanders-sketch <zachary.sanders@bsd52.org>
2026-04-14 12:42:37 -0700

    Update draw.js

---
+++ draw.js
@@ -0,0 +1,22 @@
+(function setup() {
+    snake = new Snake();
+    fruit = new Fruit();
+    fruit.pickLocation();
+
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
+        snake.update();
+        snake.draw();
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
+        snake.checkCollision();  
+        document.querySelector('.score').innerText = snake.total;
+    }, 250);
+}());
+
+window.addEventListener('keydown', ((evt) => {
+    const direction = evt.key.replace('Arrow', '');
+    snake.changeDirection(direction);
+}));
