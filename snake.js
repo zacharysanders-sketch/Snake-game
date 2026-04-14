d983e15773c19267097273fc9d3669900e915739
zacharysanders-sketch <zachary.sanders@bsd52.org>
2026-04-14 12:59:58 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,23 +1,26 @@
 function Snake() {
     this.x = 0;
     this.y = 0;
-    this.xspeed = scale;
+    this.xspeed = scale * 1;
     this.yspeed = 0;
     this.total = 0;
     this.tail = [];
 
     this.draw = function() {
-        ctx.fillStyle = "#ffffff";
-        for (let i = 0; i < this.tail.length; i++) {
+        ctx.fillStyle = "#1b7431";
+
+        for (var i = 0; i < this.tail.length; i++) {
             ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
         }
+
         ctx.fillRect(this.x, this.y, scale, scale);
     }
 
     this.update = function() {
-        for (let i = 0; i < this.tail.length - 1; i++) {
+        for (var i = 0; i < this.tail.length - 1; i++) {
             this.tail[i] = this.tail[i + 1];
         }
+
         if (this.total >= 1) {
             this.tail[this.total - 1] = { x: this.x, y: this.y };
         }
@@ -25,33 +28,34 @@ function Snake() {
         this.x += this.xspeed;
         this.y += this.yspeed;
 
+        // Wrap around edges (toroidal)
         if (this.x >= canvas.width) this.x = 0;
-        if (this.x < 0) this.x = canvas.width - scale;
         if (this.y >= canvas.height) this.y = 0;
+        if (this.x < 0) this.x = canvas.width - scale;
         if (this.y < 0) this.y = canvas.height - scale;
     }
 
     this.changeDirection = function(direction) {
         switch(direction) {
-            case "Up":
-                if (this.yspeed !== scale) {
+            case 'Up':
+                if (this.yspeed !== scale) {  // Prevent reversing directly
                     this.xspeed = 0;
                     this.yspeed = -scale;
                 }
                 break;
-            case "Down":
+            case 'Down':
                 if (this.yspeed !== -scale) {
                     this.xspeed = 0;
                     this.yspeed = scale;
                 }
                 break;
-            case "Left":
+            case 'Left':
                 if (this.xspeed !== scale) {
                     this.xspeed = -scale;
                     this.yspeed = 0;
                 }
                 break;
-            case "Right":
+            case 'Right':
                 if (this.xspeed !== -scale) {
                     this.xspeed = scale;
                     this.yspeed = 0;
@@ -66,5 +70,16 @@ function Snake() {
             return true;
         }
         return false;
+    }
+
+    this.checkCollision = function() {
+        // 
+        for (var i = 0; i < this.tail.length; i++) {
+            if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
+              
+                this.total = 0;
+                this.tail = [];
+            }
+        }
     }
 }
