bab4411cd8faf53f8001e2b62feef779cfb16c6e
zacharysanders-sketch <zachary.sanders@bsd52.org>
2026-04-14 12:42:15 -0700

    Update snake.js

---
+++ snake.js
@@ -0,0 +1,85 @@
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = scale * 1;
+    this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
+
+    this.draw = function() {
+        ctx.fillStyle = "#fff";
+
+        for (var i = 0; i < this.tail.length; i++) {
+            ctx.fillRect(this.tail[i].x, this.tail[i].y, scale, scale);
+        }
+
+        ctx.fillRect(this.x, this.y, scale, scale);
+    }
+
+    this.update = function() {
+        for (var i = 0; i < this.tail.length - 1; i++) {
+            this.tail[i] = this.tail[i + 1];
+        }
+
+        if (this.total >= 1) {
+            this.tail[this.total - 1] = { x: this.x, y: this.y };
+        }
+
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+
+        // Wrap around edges (toroidal)
+        if (this.x >= canvas.width) this.x = 0;
+        if (this.y >= canvas.height) this.y = 0;
+        if (this.x < 0) this.x = canvas.width - scale;
+        if (this.y < 0) this.y = canvas.height - scale;
+    }
+
+    this.changeDirection = function(direction) {
+        switch(direction) {
+            case 'Up':
+                if (this.yspeed !== scale) {  // Prevent reversing directly
+                    this.xspeed = 0;
+                    this.yspeed = -scale;
+                }
+                break;
+            case 'Down':
+                if (this.yspeed !== -scale) {
+                    this.xspeed = 0;
+                    this.yspeed = scale;
+                }
+                break;
+            case 'Left':
+                if (this.xspeed !== scale) {
+                    this.xspeed = -scale;
+                    this.yspeed = 0;
+                }
+                break;
+            case 'Right':
+                if (this.xspeed !== -scale) {
+                    this.xspeed = scale;
+                    this.yspeed = 0;
+                }
+                break;
+        }
+    }
+
+    this.eat = function(fruit) {
+        if (this.x === fruit.x && this.y === fruit.y) {
+            this.total++;
+            return true;
+        }
+        return false;
+    }
+
+    this.checkCollision = function() {
+        // 
+        for (var i = 0; i < this.tail.length; i++) {
+            if (this.x === this.tail[i].x && this.y === this.tail[i].y) {
+              
+                this.total = 0;
+                this.tail = [];
+            }
+        }
+    }
+}
